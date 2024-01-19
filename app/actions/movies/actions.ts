17d8457e0e8f719e070af57a8f2d1293695ad164
Imra Kocis <imrakocis@gmail.com>
2024-01-19 23:51:34 +0100

    MC-4 | removed comments

--- app/actions/movies/actions.ts
+++ app/actions/movies/actions.ts
@@ -1,13 +1,9 @@
 'use server';
 import { MovieLists } from '@/app/lib/tmdb/movies/types/movieLists';
 
-// interface Test {
-//   pageParams: number | undefined;
-// }
 export async function getPopularMovies({ pageParam = 1 }): Promise<MovieLists> {
   const url = process.env.API_BASE_URL;
   const token = process.env.API_TOKEN;
-  console.log(pageParam);
 
   const response = await fetch(
     `${url}/movie/popular?` +
